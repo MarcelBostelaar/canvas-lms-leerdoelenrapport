6a11ba04042b1b43adfbbb0399650dee1cbf1b34
Marcel Bostelaar <marcel.bostelaar@gmail.com>
2025-10-01 03:02:08 +0200

    cleanup

--- src/controllers/api/PrefetchStudentResults.php
+++ src/controllers/api/PrefetchStudentResults.php
@@ -4,19 +4,18 @@ require_once __DIR__ . '/../../services/CanvasReader.php';
 require_once __DIR__ . '/../../services/StudentProvider.php';
 require_once __DIR__ . '/APIController.php';
 class PrefetchStudentResults extends APIController {
-    protected $debug_keep_output = true;
     public function handle(){
         $canvasReader = CanvasReader::getReader();
         $studentID = (int)$_GET['id'];
         
-        // var_dump($canvasReader);
+
         $student = (new StudentProvider($canvasReader))->getByID($studentID);
         $x = $student->getMasteryResults($canvasReader);
         $y = $student->getIndividualGrades($canvasReader);
-        // return "Hello";
+
         return [
             "message" => "Data prefetched",
-            "values" => [serialize($x), serialize($y)]
+            // "values" => [serialize($x), serialize($y)]
         ];
     }
 }
